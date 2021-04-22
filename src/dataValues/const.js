bd42733055bccd69878008d16547e89ca7c9114a
shamsAlamNaimi <naimi.s@husky.neu.edu>
2021-04-22 14:26:22 -0400

    adding backend api

--- src/dataValues/const.js
+++ src/dataValues/const.js
@@ -1,5 +1,5 @@
  const varable={
-    backEndApi:`http://localhost:5000`,
+    backEndApi:`http://af495815ea3054356a067ad9ddd082ae-40158606.us-west-2.elb.amazonaws.com`,
     googleKey:'AIzaSyBp_yhd9zuT070_ZoqKc--7kcUCXzAwMT0'
 }
 
