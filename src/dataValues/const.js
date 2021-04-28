e7670f5e41415905f119bb4704cdcec87405f80e
Shams alam naimi <60454351+shamsAlamNaimi@users.noreply.github.com>
2021-04-28 12:36:06 -0400

    Update const.js
    removing google key

--- src/dataValues/const.js
+++ src/dataValues/const.js
@@ -1,7 +1,7 @@
  const varable={
     backEndApi:`http://a8dac24748d2b46bebd46fed55ef96e0-2080150944.us-west-2.elb.amazonaws.com/`,
 	//backEndApi:`http://localhost:5000`,
-    googleKey:'AIzaSyBp_yhd9zuT070_ZoqKc--7kcUCXzAwMT0'
+    googleKey:'' //removed as making the repo public 
 }
 
 export default varable;
